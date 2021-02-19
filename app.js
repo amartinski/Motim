37c238e1b0ef71c191c16c68125f6bf6c598464b
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2021-02-19 17:36:07 -0300

    0001

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let tl = new TimelineMax();
 
-    tl.fromTo('.bg-loader', 2, {width: '100%'},
+    tl.fromTo('.bg-loader', 2, 
+    {width: '100%'},
     {width: '0%',delay: 3, ease: Expo.easeInOut})
 
-    .fromTo('.bg-video', 1,
+    .fromTo('.bg-video', 5,    
     {width:'50%', opacity: 0},
-    {width: "100%", opacity: 1, ease: Expo.easeout})
+    {width: "100%", opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.navbar', 1,
     {y:-50, opacity: 0},
@@ -24,7 +25,5 @@ document.addEventListener('DOMContentLoaded', () => {
     .fromTo('.buttom', 1,
     {y:-50, opacity: 0},
     {y:0, opacity: 1, ease: Expo.easeInOut}, '-=2')
-
     
 })
-
